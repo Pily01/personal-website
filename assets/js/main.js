c4198aee4ed856e4f61b7e9f1cc91326c1a2951a
Dominguez <pilymdm@bu.edu>
2022-09-16 17:43:42 -0500

    feat: add event listener to work section nav links

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 
-
+/* M I X  I T  U P */
 var mixerPortfolio = mixitup('.work__container', {
     selectors: {
         target: '.work__card'
@@ -8,3 +8,13 @@ var mixerPortfolio = mixitup('.work__container', {
         duration: 300
     }
 });
+
+/* Active Work Group */
+const linkWork = document.querySelectorAll(".work__item");
+
+function changeActive(){
+    linkWork.forEach(link => link.classList.remove("active-work"))
+    this.classList.add("active-work")
+}
+
+linkWork.forEach(link => link.addEventListener("click", changeActive));
