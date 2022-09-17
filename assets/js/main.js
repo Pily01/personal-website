8005dabd3209c5207bce4cc915b903e132be04ae
Dominguez <pilymdm@bu.edu>
2022-09-17 13:42:47 -0500

    feat: add scroll active function for navbar active links when scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,25 @@ function changeActive(){
 }
 
 linkWork.forEach(link => link.addEventListener("click", changeActive));
+
+/* S C R O L L  R E V E A L */
+ 
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
